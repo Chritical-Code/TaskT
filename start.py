287152d02759fbcb5642ce73f24833e0f0abd570
purpleviking <purpleviking221@gmail.com>
2025-08-22 14:21:36 -0400

    Changed theme to pink, made window about 1600x900

--- start.py
+++ start.py
@@ -9,15 +9,15 @@ from sound.sound import Sound
 
 
 #init base screen/window
-os.system("mode con cols=60 lines=21")
+os.system("mode con cols=94 lines=25")
 #os.system("chcp 437")
 wholeScreen = curses.initscr()
 curses.start_color()
 
 #init screens
-headerScreen = curses.newwin(3, 60, 0, 0)
-bodyScreen = curses.newwin(15, 60, 3, 0)
-footerScreen = curses.newwin(3, 60, 18, 0)
+headerScreen = curses.newwin(3, 94, 0, 0)
+bodyScreen = curses.newwin(18, 94, 3, 0)
+footerScreen = curses.newwin(4, 94, 21, 0)
 
 #init classes
 header = Header(headerScreen)
